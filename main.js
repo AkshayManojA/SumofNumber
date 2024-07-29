7e419951da22f59e06062e2cfcfae37075a7293a
AkshayManojA <akshaymanojanadakkal@gmail.com>
2024-07-29 22:20:57 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+let num = prompt("Enter the Number")
+
+let sum = 0
+
+ for(let i = 1; i<= num ; i++){
+
+    sum = i+sum
+  }
+
+document.write("Your sum of the number upto the given number =")
+document.write(sum)
